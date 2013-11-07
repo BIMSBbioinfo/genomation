8b74123d3f0cd44b7f42cb98d3df25b3a469864b
frenkiboy <vedran.franke@gmail.com>
2013-11-07 14:54:16 +0100

    test*: 	- data files for testing
    scoreMatrix.R:
    	- examples fro binMatrix
    
    ScoreMatrixTests:
    	- tests for binMatrix

--- R/scoreMatrix.R
+++ R/scoreMatrix.R
@@ -53,15 +53,15 @@ checkClass = function(x, class.name, var.name = deparse(substitute(x))){
 # ---------------------------------------------------------------------------- #
 # given a big bam path reads the big wig file into a RleList
 # to be used by ScoreMatrix:char,GRanges
-readBam = function(target, windows, param=NULL, 
-                   unique=TRUE, extend=0, rpm=FALSE, ...){
+readBam = function(target, windows, rpm=FALSE,
+                   unique=FALSE, extend=0, param=NULL, ...){
  
   # generates the ScanBamParam object
   if(is.null(param)){
-    param <- ScanBamParam(which=reduce(windows))
+    param <- ScanBamParam(which=reduce(windows, ignore.strand=TRUE))
   }else{
     if(class(param) == 'ScanBamParam'){
-      bamWhich(param) <- reduce(windows)
+      bamWhich(param) <- reduce(windows, ignore.strand=TRUE)
     }else{
       stop('param needs to be an object of clas ScanBamParam')
     }
@@ -74,7 +74,7 @@ readBam = function(target, windows, param=NULL,
     alns = unique(alns)
   
   if(extend > 0)
-    resize(alns, width=extend)
+    alns = resize(alns, width=extend, fix='start')
   if(extend < 0)
     stop('extend needs to be a positive integer')
   
@@ -153,6 +153,11 @@ readBigWig = function(file, windows=NULL, ...){
 #'                   NA in the returned object. This useful for situations where
 #'                   you can not have coverage all over the genome, such as CpG methylation
 #'                   values.
+#' @param type if target is a character vector of file paths, then type designates the type of the corresponding files (bam or bigWig)
+#' @param rpm boolean telling whether to normalize the coverage to per milion reads. FALSE by default.
+#' @param param ScanBamParam object 
+#' @param unique boolean which tells the function to remove duplicated reads based on chr, start, end and strand
+#' @param extend numeric which tells the function to extend the reads to width=extend
 #' @param ... further arguments that control the behaviour of ScoreMatrixList on various input formats (e.g.a param argument containing a ScanBamParam object, when working with bam files)
 #' 
 #' @return returns a \code{ScoreMatrix} object
@@ -160,26 +165,38 @@ readBigWig = function(file, windows=NULL, ...){
 #' @examples
 #' 
 #' # When target is GRanges
-#'          data(cage)
-#'          data(promoters)
-#'          scores1=ScoreMatrix(target=cage,windows=promoters,strand.aware=TRUE,
-#'                                  weight.col="tpm")
-#'                                  
+#'  data(cage)
+#'  data(promoters)
+#'  scores1=ScoreMatrix(target=cage,windows=promoters,strand.aware=TRUE,
+#'                                  weight.col="tpm")                                
 #' # When target is RleList
-#' covs=coverage(cage)
-#' scores2=ScoreMatrix(target=covs,windows=promoters,strand.aware=TRUE)    
+#' covs = coverage(cage)
+#' scores2 = ScoreMatrix(target=covs,windows=promoters,strand.aware=TRUE)    
 #' 
-#'  
 #' # When target is a bam file
-#'  # bamfile="example.bam"
-#'  # scores3=ScoreMatrix(target=bamfile,windows=promoters,strand.aware=TRUE) 
+#'  bam.file = system.file('extdata/test.bam', package='genomation')
+#'  windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5))
+#'  scores3 = ScoreMatrix(target=bam.file,windows=windows, type='bam') 
+#'  
+#' # when target is a bigWig file
+#'  bw.file = system.file('extdata/test.bw', package='genomation')
+#'  windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5))
+#'  scores3 = ScoreMatrix(target=bam.file,windows=windows, type='bigWig') 
 #'  
 #' @docType methods
 #' @rdname ScoreMatrix-methods           
 #' @export
 setGeneric("ScoreMatrix",
-                    function(target,windows,strand.aware=FALSE,
-                             weight.col=NULL,is.noCovNA=FALSE,...) 
+                    function(target,windows,
+                             strand.aware=FALSE,
+                             weight.col=NULL,
+                             is.noCovNA=FALSE,
+                             type='',
+                             rpm=FALSE, 
+                             unique=FALSE, 
+                             extend=0,
+                             param=NULL,
+                             ...) 
                                 standardGeneric("ScoreMatrix") )
 
 
@@ -242,7 +259,7 @@ setMethod("ScoreMatrix",signature("RleList","GRanges"),
 #' @aliases ScoreMatrix,GRanges,GRanges-method
 #' @rdname ScoreMatrix-methods
 setMethod("ScoreMatrix",signature("GRanges","GRanges"),
-          function(target,windows,strand.aware,weight.col,is.noCovNA,...){
+          function(target, windows, strand.aware, weight.col,is.noCovNA,...){
             
             #make coverage vector  from target
             if(is.null(weight.col)){
@@ -269,12 +286,11 @@ setMethod("ScoreMatrix",signature("GRanges","GRanges"),
 })
 
 # ---------------------------------------------------------------------------- #
-#' @param type if l is a character vector of file paths, then type designates the type of the corresponding files (bam or bigWig)
-#' @param rpm boolean telling whether to normalize the coverage to per milion reads. FALSE by default.
 #' @aliases ScoreMatrix,character,GRanges-method
 #' @rdname ScoreMatrix-methods
 setMethod("ScoreMatrix",signature("character","GRanges"),
-          function(target,windows,strand.aware, type,rpm, ...){
+          function(target,windows, strand.aware, type='', 
+                   rpm=FALSE, unique=FALSE, extend=0, param=NULL, ...){
             
             if(!file.exists(target)){
 			      	stop("Indicated 'target' file does not exist\n")
@@ -282,12 +298,18 @@ setMethod("ScoreMatrix",signature("character","GRanges"),
             
             fm = c('bam','bigWig')
             if(!type %in% fm)
-              stop(paste('currently supported formats are', fm))
+              stop(paste(c('currently supported formats are', fm)))
+            
+            if(type == 'bam' & !grepl('bam$',target))
+              warning('you have set type="bam", but the designated file does not have .bam extension')
+            if(type == 'bigWig' & !grepl('bw$',target))
+              warning('you have set type="bigWig", but the designated file does not have .bw extension')
             
             if(type == 'bam')
-              covs = readBam(target, windows, ...)
+              covs = readBam(target, windows, rpm=rpm, unique=unique, 
+                             extend=extend, param=param)
             if(type == 'bigWig')
-              covs = readBigWig(target, windows, ...)            
+              covs = readBigWig(target=target, windows=windows, ...)            
             
             # get coverage vectors
             ScoreMatrix(covs,windows,strand.aware)
@@ -303,6 +325,14 @@ setMethod("ScoreMatrix",signature("character","GRanges"),
 #' @param fun \code{character} vector or an anonymous function that will be used for binning
 #'
 #' @return \code{ScoreMatrix} or \code{ScoreMatrixList} object
+#'
+#' @examples
+#' 
+#' # binning the columns in a ScoreMatrix object
+#' target = GRanges(rep(c(1,2),each=7), IRanges(rep(c(1,1,2,3,7,8,9), times=2), width=5), weight = rep(c(1,2),each=7), strand=c('-', '-', '-', '-', '+', '-', '+', '-', '-', '-', '-', '-', '-', '+'))
+#' windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5), strand=c('-','+','-','+'))
+#' sm = ScoreMatrix(target, windows)
+#' bin = binMatrix(sm, bin.num=2)
 #'
 #' @docType methods
 #' @rdname binMatrix-methods
--- inst/tests/ScoreMatrixTests.R
+++ inst/tests/ScoreMatrixTests.R
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------------- #
-# test for scoreMatrix function
-test_that("scoreMatrix works",
+# test for ScoreMatrix function
+test_that("ScoreMatrix: RleList, GRanges works",
 	{
 	
 		# input RleList
@@ -8,48 +8,51 @@ test_that("scoreMatrix works",
 		
 		#1. test for proper workings
 		gr1 = GRanges(rep(c('chr1','chr2'), each=2), IRanges(c(1,5,1,5),c(3,7,3,7)), strand=c('+','-','+','-'))
-		m1 = as(matrix(c(1,1,1,2,2,3,4,4,4,5,5,6), ncol=3, byrow=T), 'scoreMatrix')
+		m1 = as(matrix(c(1,1,1,2,2,3,4,4,4,5,5,6), ncol=3, byrow=T), 'ScoreMatrix')
 		rownames(m1) = 1:4
-		expect_equal(scoreMatrix(rl, gr1, strand.aware = FALSE), m1)
+		expect_equal(ScoreMatrix(rl, gr1, strand.aware = FALSE), m1)
 		
-		m2 = as(matrix(c(1,1,1,3,2,2,4,4,4,6,5,5), ncol=3, byrow=T), 'scoreMatrix')
+		m2 = as(matrix(c(1,1,1,3,2,2,4,4,4,6,5,5), ncol=3, byrow=T), 'ScoreMatrix')
 		rownames(m2) = 1:4
-		expect_equal(scoreMatrix(rl, gr1, strand.aware = TRUE), m2)
+		expect_equal(ScoreMatrix(rl, gr1, strand.aware = TRUE), m2)
 		
 		#2. test for different lengths
 		gr2 = GRanges(rep(c('chr1','chr2'), each=2), IRanges(c(1,5,1,5),c(3,7,3,9)))
-		expect_error(scoreMatrix(rl, gr2), "width of 'windows' are not equal, provide 'windows' with equal widths")
+		expect_error(ScoreMatrix(rl, gr2), "width of 'windows' are not equal, provide 'windows' with equal widths")
 		
 		#3. test for removing outliers
 		gr3 = GRanges(rep(c('chr1','chr2'), each=2), IRanges(c(1,5,8,1),c(3,7,10,3)))
-		m3 = as(matrix(c(1,1,1,2,2,3,4,4,4), ncol=3, byrow=T), 'scoreMatrix')
+		m3 = as(matrix(c(1,1,1,2,2,3,4,4,4), ncol=3, byrow=T), 'ScoreMatrix')
 		rownames(m3) = c(1,2,4)
-		expect_equal(scoreMatrix(rl, gr3), m3)
+		expect_equal(ScoreMatrix(rl, gr3), m3)
 		
 		#4. test for different chromosomes
 		gr4 = GRanges(rep(c('chr1','chr3'), each=2), IRanges(c(1,5,1,5),c(3,7,3,7)))
-		m4 = as(matrix(c(1,1,1,2,2,3), ncol=3, byrow=T), 'scoreMatrix')
+		m4 = as(matrix(c(1,1,1,2,2,3), ncol=3, byrow=T), 'ScoreMatrix')
 		rownames(m4) = 1:2
-		expect_equal(scoreMatrix(rl, gr4, strand.aware = FALSE), m4)
+		expect_equal(ScoreMatrix(rl, gr4, strand.aware = FALSE), m4)
 		
 		#5. no overlapping chromosomes
 		gr5 = GRanges(rep(c('chr3','chr4'), each=2), IRanges(c(1,5,1,5),c(3,7,3,7)))
-		expect_error(scoreMatrix(rl, gr5), "All windows fell have coordinates outside chromosome boundaries")
+		expect_error(ScoreMatrix(rl, gr5), "All windows fell have coordinates outside chromosome boundaries")
 		
 		#6. only windows that fall off chromosomes
 		gr6 = GRanges(rep(c('chr1','chr4'), each=2), IRanges(c(-1,8,1,5),c(1,10,3,7)))
-		expect_error(scoreMatrix(rl, gr6), "All windows fell have coordinates outside chromosome boundaries")
+		expect_error(ScoreMatrix(rl, gr6), "All windows fell have coordinates outside chromosome boundaries")
 		
 	})
 
 # ---------------------------------------------------------------------------- #
-test_that("scoreMatrix:GRanges, GRanges works",
+test_that("ScoreMatrix:GRanges, GRanges works",
 {
-          target = GRanges(rep(c(1,2),each=6), IRanges(rep(c(1,2,3,7,8,9), times=2), width=5))
-          values(target)$weight = rep(c(1,2),each=6)
-          windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5))
+          target = GRanges(rep(c(1,2),each=6), 
+                           IRanges(rep(c(1,2,3,7,8,9), times=2), width=5),
+                           values(target)$weight = rep(c(1,2),each=6))
+          windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5), 
+                            strand=c('-','+','-','+'))
           
-          
+          # -----------------------------------------------#
+          # usage
           # normal function works
           s1 = ScoreMatrix(target=target, windows=windows)
           m1 = matrix(rep(c(1,2,3,3,3,2,3,3,3,2),times=2), ncol=5, byrow=T)
@@ -63,34 +66,94 @@ test_that("scoreMatrix:GRanges, GRanges works",
           m2[3:4,] = m2[3:4,]*2
           rownames(m2) = 1:4
           m2 = as(m2, 'ScoreMatrix')
-          expect_equal(s2,m2)      
+          expect_equal(s2,m2)  
+          
+          #strand aware
+          s3 = ScoreMatrix(target=target, windows=windows, strand.aware=T)
+          m3 = matrix(rep(c(1,2,3,3,3,2,3,3,3,2),times=2), ncol=5, byrow=T)
+          rownames(m3) = 1:4
+          m3[c(1,3),] = rev(m3[c(1,3),])
+          m3 = as(m3, 'ScoreMatrix')
+          expect_equal(s3,m3)
+          
+          # -----------------------------------------------#
+          # errors
+          expect_error(ScoreMatrix(target, windows, weight.col=''))
+          
 })
 
 
 # ---------------------------------------------------------------------------- #
-test_that("scoreMatrix:character, GRanges works",
+test_that("ScoreMatrix:character, GRanges works",
 {
-  target = GRanges(rep(c(1,2),each=6), IRanges(rep(c(1,2,3,7,8,9), times=2), width=5))
-  values(target)$weight = rep(c(1,2),each=6)
-  windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5))
+  target = GRanges(rep(c(1,2),each=7), 
+                   IRanges(rep(c(1,1,2,3,7,8,9), times=2), width=5),
+                   weight = rep(c(1,2),each=7), 
+                   strand=c('-', '-', '-', '-', '+', '-', '+', '-', '-', '-', '-', '-', '-', '+'))
+  windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5), 
+                    strand=c('-','+','-','+'))
   
- 
+  # -----------------------------------------------#
+  # usage
+  # bam file
+  bam.file = system.file('extdata/test.bam', package='genomation')
+  s1 = ScoreMatrix(bam.file, windows, type='bam')
+  m1 = ScoreMatrix(target, windows)
+  expect_equal(s1,m1)
   
-  # function with weight col works
-  s2 = ScoreMatrix(target=target, windows=windows, weight.col='weight')
-  m2 = matrix(rep(c(1,2,3,3,3,2,3,3,3,2),times=2), ncol=5, byrow=T)
-  m2[3:4,] = m2[3:4,]*2
-  rownames(m2) = 1:4
-  m2 = as(m2, 'ScoreMatrix')
-  expect_equal(s2,m2)
+  # bam file, rpm=TRUE
+  s2 = ScoreMatrix(bam.file, windows, type='bam', rpm=TRUE)
+  tot = 1e6/countBam(BamFile(bam.file))$records
+  m2 = m1*tot
+  expect_equal(s2, m2)
   
+  #bam file, rpm=FALSE, unique=TRUE
+  s3 = ScoreMatrix(bam.file, windows, type='bam', unique=T)
+  m3 = ScoreMatrix(unique(target), windows)
+  expect_equal(s3,m3)
+  
+  #bam file, rpm=FALSE, unique=TRUE, extend=1
+  s4 = ScoreMatrix(bam.file, windows, type='bam', unique=T, extend=1)
+  m4 = ScoreMatrix(resize(unique(target), width=1), windows)
+  expect_equal(s4,m4)
+  
+  #bigWig file - does not work on windows
+  #bw.file = system.file('extdata/test.bw', package='genomation')
+  #s5 = ScoreMatrix(bw.file, windows, type='bigWig')
+  #m5 = ScoreMatrix(target, windows)
+  #expect_equal(s1,m1)
+  # -----------------------------------------------#
+  # errors
+  # error upon not specifying the file
+  expect_error(ScoreMatrix('',windows))
+  
+  # error upon not specifying the format
+  expect_error(ScoreMatrix(bam.file, target))
+  
+})
+
+
+# ---------------------------------------------------------------------------- #
+test_that("binMatrix works",
+{
+  target = GRanges(rep(c(1,2),each=7), 
+                   IRanges(rep(c(1,1,2,3,7,8,9), times=2), width=5), 
+                   weight = rep(c(1,2),each=7), 
+                   strand=c('-', '-', '-', '-', '+', '-', '+', '-', '-', '-', '-', '-', '-', '+'))
+  windows = GRanges(rep(c(1,2),each=2), IRanges(rep(c(1,2), times=2), width=5), strand=c('-','+','-','+'))
+  s = ScoreMatrix(target, windows)
+  bin = binMatrix(sm, bin.num=2)
+  
+  m = as(cbind(rowMeans(s[,1:3]), rowMeans(s[,3:5])),'ScoreMatrix')
+  expect_equal(bin, m)
   
   
 })
+
 	
 # ---------------------------------------------------------------------------- #
-# test for removeOffRanges
-test_that("constrainRanges work",
+# test for constrainRanges
+test_that("constrainRanges works",
 	{
 		target= RleList(chr1 = Rle(rep(c(1,2,3), each=3)), chr2=Rle(rep(c(4,5,6), each=3)))
 		gr1 = GRanges(rep(c('chr1','chr2'), each=2), IRanges(c(1,5,1,8),c(3,7,3,10)))
@@ -102,21 +165,21 @@ test_that("constrainRanges work",
 # ---------------------------------------------------------------------------- #
 # test for binMatrix
 test_that("binMatrix works"
-	m1 = as(matrix(rep(1:4, 4), ncol=4), 'scoreMatrix')
+	m1 = as(matrix(rep(1:4, 4), ncol=4), 'ScoreMatrix')
 	
 	# no binning
 	expect_identical(binMatrix(m1), m1)
 	
 	# nbins 2, default function
-	mb1 = as(matrix(c(1,1,2,2,3,3,4,4), ncol=2, byrow=T), 'scoreMatrix')
+	mb1 = as(matrix(c(1,1,2,2,3,3,4,4), ncol=2, byrow=T), 'ScoreMatrix')
 	expect_identical(binMatrix(m1, nbins=2), mb1)
 	
 	# nbins 5
 	expect_error(binMatrix(m1, nbins=5), "number of given bins is bigger than the number of matrix columns")
 	
 	# nbins 2, not default function
-	m2 = as(matrix(rep(1:4, 4), ncol=4, byrow=T), 'scoreMatrix')
-	mb2 = as(matrix(rep(c(2,4), 4), ncol=2, byrow=T), 'scoreMatrix')
+	m2 = as(matrix(rep(1:4, 4), ncol=4, byrow=T), 'ScoreMatrix')
+	mb2 = as(matrix(rep(c(2,4), 4), ncol=2, byrow=T), 'ScoreMatrix')
 	expect_equal(binMatrix(m2, nbins=2, max), mb2)	
 )
 
