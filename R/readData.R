1e6873cfac8a450b81de56551872daf2278937af
frenkiboy <vedran.franke@gmail.com>
2013-11-08 14:17:47 +0100

    DESCRIPTION: 	- removed gffFunctions.R
    readData.R:
    	- added gffToGRanges

--- R/readData.R
+++ R/readData.R
@@ -319,61 +319,54 @@ setMethod("readTranscriptFeatures",
 
 # ---------------------------------------------------------------------------- #
 #' Converts a gff formated data.frame into a GenomicRanges object. 
-#' The GenomicRanges object needs to be properly formated for the function to work. If some of the ranges have negative width, then the function will issue a warning (happens sometimes with ranges downloaded from Ensembl)
+#' The GenomicRanges object needs to be properly formated for the function to work.
 #' @param gff.file a path to a gff formatted file
 #' @param filter a character designating which elements to retain from the gff file (e.g. exon, CDS, ...)
 #' @return returns a \code{GenomicRanges} object
 #' 
 #' @examples
 #' gff.file = system.file('extdata/chr21.refseq.hg19.gtf', package='genomation')
-#' gffToGRanges(gff.file)
+#' gff = gffToGRanges(gff.file)
 #' 
 #' 
 #' @docType methods
 #' @export
-gffToGRanges = function(gff.file, filter=NULL){
+gffToGRanges = function(gff.file, split.group=FALSE, split.char=';',filter=NULL){
   
-  gff = readGeneric(gff.file)
+  gff = readGeneric(gff.file, 
+                    chr=1,
+                    start=4,
+                    end=5, 
+                    strand=7,
+                    meta.col=list(source=2,
+                                  feature=3,
+                                  score=6,
+                                  frame=8,
+                                  group=9))
   
-  if(ncol(gff) != 9)
-    stop("Number of columns does not match gff format")
-  
-  if(any(gff[,5] < gff[,4])){
-    warning("gff file contains ranges with negative widths...")
-    gff = gff[gff[,5] > gff[,4],]
+  if(split.group){
+    message('splitting the group.column...')
+    group = strsplit(gff$group, '\\s+')
+    gnames = group[[1]][seq(1,length(group[[1]]),2)]
+    gids = seq(2,length(group[[1]]),2)
+    group = data.frame(do.call(rbind, 
+                              lapply(group, 
+                                     function(x)gsub(split.char,'',x[gids]))), 
+                       stringsAsFactors=FALSE)
+    colnames(group) = gnames
+    gff$group = NULL
+    values(gff) = cbind(values(gff), group)
   }
   
   if(!is.null(filter)){		
-    if(filter %in% gff[,3]){
+    if(filter %in% gff$feature){
       message("Filtering", filter, "features...\n")
-      gff = gff[gff[,3] == filter,]
+      gff = gff[gff$feature == filter,]
     }else{
       stop("The given feature is not present in the gff file")
     }
   }
   
-  message('Getting the feature ids...\n')
-  s = strsplit(gff$V9, split=';')
-  z = sapply(s, length)
-  a = split(s, z)
-  gff = gff[order(z),]
-  l = lapply(a, function(x){
-    d = sub('^ ','', unlist(x, use.names=F))
-    d = sub('^.+? ','',d)
-    m = matrix(d, ncol = length(x[[1]]), byrow=T)
-    colnames(m) = sub(' .+$','',sub('^ ','', x[[1]]))
-    m})
-  ids = rbind.fill(lapply(l, data.frame))
-  
-  message('Constructing the granges...\n')
-  gff$V7[!gff$V7 %in% c('+','-')] = '*'
-  granges = GRanges(seqnames = gff[,1],
-                    IRanges(gff[,4],gff[,5]),
-                    strand = gff[,7],
-                    frame = gff[,8],
-                    feature.type = gff[,3])
-  
-  values(granges) = cbind(values(granges), DataFrame(ids))
-  return(granges)			
+  return(gff)
 }
 
