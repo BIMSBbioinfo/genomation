d75056973222bb7d048dc3cd7023a4bdb90b4bef
katwre <katwre@gmail.com>
2015-09-22 18:46:26 +0200

    start working on windows=GRanges

--- R/patternMatrix.R
+++ R/patternMatrix.R
@@ -46,7 +46,7 @@
 
 setGeneric(
   name="patternMatrix",
-  def=function(pattern, windows, min.score = NULL, cores=1, asPercentage=FALSE){
+  def=function(pattern, windows, genome=NULL, min.score = NULL, cores=1, asPercentage=FALSE){
     standardGeneric("patternMatrix")
   }
 )
@@ -128,6 +128,77 @@ setMethod("patternMatrix",
             new("ScoreMatrix",mat)
 
           }
+)
+
+
+
+setMethod("patternMatrix",
+          signature(pattern = "matrix", windows = "GRanges"),
+          function(pattern, windows, genome, min.score = NULL){
+            
+            if(!(length(unique(width(windows))) == 1)){
+              stop("All sequences in the input DNAStringSet must have the same 
+                   length!")
+            }
+            
+            source("https://bioconductor.org/biocLite.R")
+            biocLite("BSgenome.Hsapiens.UCSC.hg19")
+            library(BSgenome.Hsapiens.UCSC.hg19)
+            
+            genome = BSgenome.Hsapiens.UCSC.hg19
+            
+            seqinfo <- Seqinfo(paste0("chr", 1:3), c(1000, 2000, 1500), NA, "mock1")
+            gr <-
+              GRanges(seqnames =
+                        Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+                      ranges = IRanges(
+                        1:10, width = 10:1, names = head(letters,10)),
+                      strand = Rle(
+                        strand(c("-", "+", "*", "+", "-")),
+                        c(1, 2, 2, 3, 2)),
+                      score = 1:10,
+                      GC = seq(1, 0, length=10),
+                      seqinfo=seqinfo(genome))
+            
+            seqinfo(gr) <- seqinfo(genome)
+            
+            windows=gr
+            
+            #Error in .starfreeStrand(strand(names)) : 
+            #cannot mix "*" with other strand values
+            strand(windows) <- c("*")
+  
+            #TODO: check if windows are not out of chromosomes?
+            windows = getSeq(genome, windows)
+            
+            # e.g. when windows="hg19"
+            #if(class(windows)=="character"){
+              ##genome: A string identifying a genome, usually one assigned by UCSC, like "hg19"
+              #windows = GRangesForUCSCGenome(genome, chrom = NULL, ranges = NULL)
+            #}
+            
+            if(is.null(min.score)){
+              mat <- motifScanScores(regionsSeq = windows,
+                                     motifPWM = pattern,
+                                     asPercentage = asPercentage)
+            }else{
+              
+              pwm.match <- lapply(1:length(windows), function(x){
+                .scan.sequence.with.pwm(pwm = pattern, seq = toString(windows[x]), minScore=min.score) 
+              })
+              
+              #ma <- lapply(1:length(windows), function(x) as.vector(coverage(pwm.match[[x]])) )
+              # calculate coverage of occurence of each pattern, 1-present, 0-absent
+              ma <- lapply(1:length(windows), function(x) as.vector(coverage(reduce(pwm.match[[x]]))) )
+              
+              # convert list to matrix
+              mat <- matrix(unlist(ma), ncol = length(ma[[1]]), byrow = TRUE)
+              
+            }
+            
+            new("ScoreMatrix",mat)
+            
+            }
 )
 
 
--- inst/unitTests/test_PatternMatrix.R
+++ inst/unitTests/test_PatternMatrix.R
@@ -23,3 +23,38 @@ test_PatternMatrix_DNAStringSet_DNAStringSet = function()
   p = patternMatrix(pattern=pattern, windows=windows)
   
 }
+
+
+test_PatternMatrix_matrix_BSgenome = function()
+{
+  
+  #TODO
+  
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("BSgenome.Hsapiens.UCSC.hg19")
+  library(BSgenome.Hsapiens.UCSC.hg19)
+  
+  genome = BSgenome.Hsapiens.UCSC.hg19
+  
+  seqinfo <- Seqinfo(paste0("chr", 1:3), c(1000, 2000, 1500), NA, "mock1")
+  gr <-
+    GRanges(seqnames =
+              Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+            ranges = IRanges(
+              1:10, width = 10:1, names = head(letters,10)),
+            strand = Rle(
+              strand(c("-", "+", "*", "+", "-")),
+              c(1, 2, 2, 3, 2)),
+            score = 1:10,
+            GC = seq(1, 0, length=10),
+            seqinfo=seqinfo(genome))
+  
+  seqinfo(gr) <- seqinfo(genome)
+  
+  #Error in .starfreeStrand(strand(names)) : 
+  #cannot mix "*" with other strand values
+  strand(gr) <- c("*")
+  
+  
+  
+}
