31decbd3f6d2719d9ed64e365d24db901ee0a89d
bozmik <bozena.mika1@gmail.com>
2017-08-22 17:00:45 +0200

    new version of binMean(), small improvement in listSliceMean()

--- binSum.cpp
+++ binSum.cpp
@@ -196,6 +196,42 @@ NumericMatrix  listSliceMean(List xlist,int n) {
   return res;
 }
 
+// [[Rcpp::export]]
+NumericVector binMean2(NumericVector x,int n) {
+  int p = x.size();  // get the length of the input vector
+  double w_size=double(p-1)/double(n); // window size can be a double
+  int step = ceil(w_size);
+  NumericVector res(n);// create the output vector
+  
+  NumericMatrix bin(step,n);
+  
+  for (int i = 0; i < p; i++){
+    bin[i] = x[i];
+  }
+  
+  NumericVector bz(step);
+  
+  for(int z = 0; z < n; z++){
+    bz = bin( _, z);
+    res[z] = std::accumulate(bz.begin(), bz.end(), 0.0)/bz.size();
+  } 
+  
+  return res;
+}
+
+
+
+// [[Rcpp::export]]
+NumericMatrix  listSliceMean2(List xlist,int n) {
+  int m = xlist.size(); 
+  NumericMatrix res(m, n);
+  NumericVector  subVec;
+  for (int i = 0; i < m; i++) {
+    subVec = binMean2(xlist[i],n);
+    res(i, _) = subVec;
+  }
+  return res;
+}
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
@@ -209,5 +245,7 @@ binMax(1:35,10)
 
 listSliceSum(list(1:35,1:25),10)
 listSliceMean(list(1:35,1:25),10)
+listSliceMean2(list(1:35,1:25),10)
 listSliceMean(list(c(120,1:35,120),1:25),10)
+listSliceMean2(list(c(120,1:35,120),1:25),10)
 */
